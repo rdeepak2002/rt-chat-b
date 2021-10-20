b4a1883bca613b57b7c24b9030c044e0c632b5c5
Darzuo <51324445+Darzuo@users.noreply.github.com>
2021-10-19 20:06:09 -0400

    Added pages and routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+
+import HomePage from './components/HomePage/home.js';
+import LoginPage from './components/LoginPage/login.js';
+import ProfilePage from './components/ProfilePage/profile.js';
+
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+
+
+
+
+      <Switch>
+        <Route path = "/login">
+          <LoginPage />
+        </Route>
+        <Route path = "/home">
+          <HomePage />
+        </Route>
+        <Route path = "/profile">
+          <ProfilePage />
+        </Route>
+      </Switch>
+
+    </Router>
   );
 }
 
