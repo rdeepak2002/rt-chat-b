d6457fc4881660f02ab01d0b1b113955fb5f0814
Darzuo <51324445+Darzuo@users.noreply.github.com>
2021-10-26 21:17:11 -0400

    More work on home pages

--- src/components/DirectChats/direct.js
+++ src/components/DirectChats/direct.js
@@ -1,4 +1,5 @@
 import "./style.css";
+import defaultpfp from "../../assets/default.jpg";
 
 const IndividualChatComponent = () =>{
     return (
@@ -6,19 +7,19 @@ const IndividualChatComponent = () =>{
             <div className = "home-header">
                 <div className = "nav-tabs">
                     <div className = "direct-tab">
-                        
+                        <button className = "direct-button" active>Direct Messages</button>
                     </div>
                     <div className = "group-tab">
-
+                        <button className = "group-button">Groups</button>
                     </div>
                 </div>
                 <div className = "profile-button-container">
-                    <button className = "profile-button"><img src="../assets/default.jpg"></img></button>
+                    <button className = "profile-button"><img id = "pfp" src={defaultpfp}></img></button>
                 </div>
             </div>
 
             <div className = "chat-previews-container">
-
+                <ChatPreviewComponent chatName = "Person A"></ChatPreviewComponent>
             </div>
         </div>
     );
